1ea7edfec1668fd5b1d124df6579b441509d8324
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2022-11-19 12:04:33 -0300

    refactor: limpeza de código

--- src/lib/component/DrawerMenu/DrawerMenu.svelte
+++ src/lib/component/DrawerMenu/DrawerMenu.svelte
@@ -1,16 +1,23 @@
 <script>
 	import DrawerMenuItem from "./DrawerMenuItem.svelte";
 
+    const items = [
+        {name: "Dashboard", path: "/"},
+        {name: "Vendas", path: "/sales"},
+        {name: "Produtos", path: "/items"},
+    ]
+
 </script>
 <div class="drawer_menu">
-    <DrawerMenuItem name="Dashboard"/>
-    <DrawerMenuItem name="Vendas"/>
-    <DrawerMenuItem name="Produtos"/>    
+    {#each items as item}
+        <DrawerMenuItem {...item} />
+    {/each}
+     
 </div>
 
 <style>
     .drawer_menu {
-        height: 100%;
+        height: 100vh;
         width: 210px;
         background-color: blueviolet;
         padding-left: 15px;
