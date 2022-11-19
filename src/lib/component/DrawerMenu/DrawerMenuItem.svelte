1111a07eea81314c4e83841fe5f3e927fdc3df04
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2022-11-19 12:05:02 -0300

    feat: adição do referencial de página

--- src/lib/component/DrawerMenu/DrawerMenuItem.svelte
+++ src/lib/component/DrawerMenu/DrawerMenuItem.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
+    import { page } from '$app/stores';
     export let name: string;
+    export let path: string;    
 </script>
 
-<div class="drawer_menu_item">{name}</div>
+<div 
+    class="drawer_menu_item" 
+    aria-current={$page.url.pathname === path ? 'page' : undefined}
+>{name}</div>
 
 <style>
     .drawer_menu_item {
         display: flex;
         align-items: center;
         height: 40px;
-        width: 100vh;
+        width: 100%;
         color: white;     
+    }
+
+    .drawer_menu_item[aria-current='page'] {
+        background-color: chocolate;
     }
 </style>
