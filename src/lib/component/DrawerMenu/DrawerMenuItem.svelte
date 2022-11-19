930548834d6603d2c889160cb03292b82f1d47ce
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2022-11-19 11:41:03 -0300

    feat: criação do DrawerMenuItem

---
+++ src/lib/component/DrawerMenu/DrawerMenuItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<div class="drawer_menu_item">{name}</div>
+
+<style>
+    .drawer_menu_item {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        width: 100vh;
+        color: white;     
+    }
+</style>
