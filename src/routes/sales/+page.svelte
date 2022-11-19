63a5ccd399934851a1e6872366a4da376d9ac612
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2022-11-19 19:43:57 -0300

    feat: adição da tabela

--- src/routes/sales/+page.svelte
+++ src/routes/sales/+page.svelte
@@ -1,1 +1,27 @@
 <h1>Vendas</h1>
+<table>
+    <tr>
+        <th>Nº Pedido</th>
+        <th>Data</th>
+        <th>Valor (R$)</th>
+        <th>Tipo de Entrega</th>
+        <th>Status</th>
+        <th>Ações</th>
+    </tr>
+    <tr>
+        <td>0001</td>
+        <td>19/11/2022</td>
+        <td>150,00</td>
+        <td>Em Casa</td>
+        <td>Aguardando Pgto.</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>0001</td>
+        <td>19/11/2022</td>
+        <td>150,00</td>
+        <td>Buscar Loja</td>
+        <td>Aguardando Pgto.</td>
+        <td></td>
+    </tr>
+</table>
