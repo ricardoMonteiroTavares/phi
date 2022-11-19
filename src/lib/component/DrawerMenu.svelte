6251c19c700a343572ac0324f0a8c060f343d53d
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2022-11-19 11:40:41 -0300

    refactor: mudança de pasta do DrawerMenu

--- src/lib/component/DrawerMenu.svelte
+++
@@ -1,7 +0,0 @@
-<div>
-    <ul>
-        <li>Dashboard</li>
-        <li>Vendas</li>
-        <li>Produtos</li>
-    </ul>
-</div>
---
+++ src/lib/component/DrawerMenu/DrawerMenu.svelte
@@ -0,0 +1,19 @@
+<script>
+	import DrawerMenuItem from "./DrawerMenuItem.svelte";
+
+</script>
+<div class="drawer_menu">
+    <DrawerMenuItem name="Dashboard"/>
+    <DrawerMenuItem name="Vendas"/>
+    <DrawerMenuItem name="Produtos"/>    
+</div>
+
+<style>
+    .drawer_menu {
+        height: 100%;
+        width: 210px;
+        background-color: blueviolet;
+        padding-left: 15px;
+        padding-right: 15px;
+    }    
+</style>
