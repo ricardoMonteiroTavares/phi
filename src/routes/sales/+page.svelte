4cfd3b43517f2448480d62da101dcca43c5e33f6
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2022-11-19 19:45:23 -0300

    feat: adição do svelte head

--- src/routes/sales/+page.svelte
+++ src/routes/sales/+page.svelte
@@ -1,27 +1,34 @@
-<h1>Vendas</h1>
-<table>
-    <tr>
-        <th>Nº Pedido</th>
-        <th>Data</th>
-        <th>Valor (R$)</th>
-        <th>Tipo de Entrega</th>
-        <th>Status</th>
-        <th>Ações</th>
-    </tr>
-    <tr>
-        <td>0001</td>
-        <td>19/11/2022</td>
-        <td>150,00</td>
-        <td>Em Casa</td>
-        <td>Aguardando Pgto.</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>0001</td>
-        <td>19/11/2022</td>
-        <td>150,00</td>
-        <td>Buscar Loja</td>
-        <td>Aguardando Pgto.</td>
-        <td></td>
-    </tr>
-</table>
+<svelte:head>
+	<title>Vendas</title>
+	<meta name="description" content="Ver todas as vendas no sistema" />
+</svelte:head>
+
+<section>
+    <h1>Vendas</h1>
+    <table>
+        <tr>
+            <th>Nº Pedido</th>
+            <th>Data</th>
+            <th>Valor (R$)</th>
+            <th>Tipo de Entrega</th>
+            <th>Status</th>
+            <th>Ações</th>
+        </tr>
+        <tr>
+            <td>0001</td>
+            <td>19/11/2022</td>
+            <td>150,00</td>
+            <td>Em Casa</td>
+            <td>Aguardando Pgto.</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>0001</td>
+            <td>19/11/2022</td>
+            <td>150,00</td>
+            <td>Buscar Loja</td>
+            <td>Aguardando Pgto.</td>
+            <td></td>
+        </tr>
+    </table>
+</section>
